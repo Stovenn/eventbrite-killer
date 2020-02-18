aebcd31588d7b12d85e5e6bf3aed4dd01bb7311d
Jonathan <Jonathan.sylvestre.pro@gmail.com>
2020-02-18 19:36:27 +0100

    first commit

---
+++ app/models/event.rb
@@ -0,0 +1,11 @@
+class Event < ApplicationRecord
+    has_and_belongs_to_many :users
+
+    validates :start_date, :duration, :title, :description, :price, :location, presence: true
+
+    # manque la validation sur multiple de 5
+    validates :duration, numericality:{greater_than_or_equal_to: 0} 
+    validates :title, length: { minimum: 5 }, length: {maximum: 140}
+    validates :description, length: { minimum: 20 }, length: {maximum: 1000}
+    validates :price, numericality: { less_than:1000 }, numericality: { more_than:1 }
+end
