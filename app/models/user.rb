417197d1b5617dd671377f0f6a9a5dcdc82f2ea6
Jonathan <Jonathan.sylvestre.pro@gmail.com>
2020-02-18 20:45:37 +0100

    mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-    has_and_belongs_to_many :events
+  has_and_belongs_to_many :events
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
 end
